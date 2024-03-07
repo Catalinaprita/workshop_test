70dea863fd60f31de796eacb743f014fdd1de9b6
Catalinaprita <catalinaprita2@gmail.com>
2024-03-07 16:48:20 +0100

    primo aggiornamento

---
+++ src/components/CardMusica.svelte
@@ -0,0 +1,55 @@
+<script>
+	export let disco;
+	export let artista;
+	export let durata;
+	export let anno;
+	export let immagine;
+</script>
+
+<div class="card">
+	<img class="card_immagine" src={immagine} alt="Copertina del disco" />
+
+	<div class="card_musica_centro">
+		<p>{artista}</p>
+		<p class="card_disco_titolo">{disco}</p>
+	</div>
+
+	<div class="card_musica_footer">
+		<p>{durata}</p>
+		<p>{anno}</p>
+	</div>
+</div>
+
+<style>
+	.card {
+		background-color: rgb(210, 70, 189);
+		width: 250px;
+		border-radius: 20px;
+		overflow: hidden;
+		padding: 20px;
+		color: white;
+		font-family: Verdana, Geneva, Tahoma, sans-serif;
+	}
+
+	.card_disco_titolo {
+		font-size: 25px;
+	}
+
+	.card_immagine {
+		object-fit: cover;
+		width: 100%;
+		border-radius: 10px;
+		aspect-ratio: 1;
+	}
+
+	.card_musica_centro {
+		margin-top: 20px;
+		margin-bottom: 20px;
+	}
+
+	.card_musica_footer {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,41 @@
-<script lang="ts">
-	import { get_collection } from '$cms';
+<script>
+	import CardMusica from '$components/CardMusica.svelte';
+
+	let dischi = [
+		{
+			artista: 'Galeffi',
+			disco: 'Tazza di te',
+			durata: '3:71',
+			anno: '2010',
+			immagine: 'https://i.scdn.co/image/ab67616d0000b273d5490bf80ec7c5f97f57d3d5'
+		},
+		{
+			artista: 'Frah Quintale',
+			disco: 'Missili',
+			durata: '4:15',
+			anno: '2018',
+			immagine:
+				'https://64.media.tumblr.com/fadc3285ed2f0b9479225f3bc9fa6b65/tumblr_p7te6cr0Wa1rt4h09o1_1280.jpg'
+		},
+		{
+			artista: 'Calcutta',
+			disco: 'Saliva',
+			durata: '2:40',
+			anno: '2017',
+			immagine: 'https://cdn.skuola.net/w1200h687/news_foto/2018/calcutta-evergreen.jpg'
+		}
+	];
 </script>
 
-Home page
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@3.4.1/src/css/preflight.css" />
+</svelte:head>
+{#each dischi as disco}
+	<CardMusica
+		disco={disco.disco}
+		artista={disco.artista}
+		durata={disco.durata}
+		anno={disco.anno}
+		immagine={disco.immagine}
+	/>
+{/each}
